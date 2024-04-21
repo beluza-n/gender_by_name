fbfa91b070902173ade07b320e1623412e85c91e
beluza-n <beluza@mail.ru>
2024-04-22 00:09:28 +0300

    model admin

--- names/admin.py
+++ names/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Man, Woman
 
-admin.site.register(Man)
-admin.site.register(Woman)
+
+class ManAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    ordering = ['name']
+
+
+class WomanAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    ordering = ['name']
+
+
+admin.site.register(Man, ManAdmin)
+admin.site.register(Woman, WomanAdmin)
